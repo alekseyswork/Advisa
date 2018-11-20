a3173732105a94b84ef5cb386e34d92a42db26ed
Alex <alekseys.workemail@gmail.com>
2018-11-20 22:00:37 +0200

    Added styles - bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForecastService } from './services/forecast.service';
-import { ForecastComponent } from './components/forecast/forecast.component';
+
 import { ListforecastsComponent } from './components/listforecasts/listforecasts.component';
 import { ManageforecastComponent } from './components/manageforecast/manageforecast.component';
+import { ForecastComponent } from './components/listforecasts/forecast/forecast.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import { ManageforecastComponent } from './components/manageforecast/manageforec
     ForecastComponent,
     ListforecastsComponent,
     ManageforecastComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
