6f987a24a43352a88677fba445fda32105a0eda5
Oleksii Samofalov <oleksii.samofalov@caspio.com>
2018-12-05 17:49:23 +0200

    Maybe better route

--- backAdvisa/lib/app.ts
+++ backAdvisa/lib/app.ts
@@ -1,43 +1,61 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { AccountRoutes } from "./routes/AccountRoutes";
+import {AccountRoutes} from "./routes/AccountRoutes";
 import * as mongoose from "mongoose";
-import { Config } from './configuration/Config';
+import {Config} from './configuration/Config';
+import { Router } from 'express';
 var cookieParser = require('cookie-parser');
 
 class App {
 
-    public app: express.Application;
-    public routePrv: AccountRoutes = new AccountRoutes();
-    public mongoUrl: string = Config.ConnectionStringDB;
+    public app : express.Application;
+    public accountRoute : AccountRoutes ;
+    public mongoUrl : string = Config.ConnectionStringDB;
+    public api:Router;
 
     constructor() {
         this.app = express();
-        this.config();        
-        this.routePrv.routes(this.app);     
+        this.api =express.Router();
+        
+        this.config();
+        this.accountRoute= new AccountRoutes(this.api);
+        this.app.use(this.api);
         this.mongoSetup();
     }
 
-    private config(): void{
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-        this.app.use(cookieParser());
-        this.app.use(function (req, res, next) {
-            // res.setHeader('Access-Control-Allow-Origin', '*');
-            // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            // res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-            // res.setHeader('Content-Type', 'application/json');
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Credentials", "true");
-            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-            
-            next();
-        });
+    private config() : void {
+        this
+            .app
+            .use(bodyParser.json());
+        this
+            .app
+            .use(bodyParser.urlencoded({extended: false}));
+        this
+            .app
+            .use(cookieParser());
+        this
+            .app
+            .use(function (req, res, next) {
+                // res.setHeader('Access-Control-Allow-Origin', '*');
+                // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,
+                // Content-Type, Accept'); res.setHeader('Access-Control-Allow-Methods', 'POST,
+                // GET, PATCH, DELETE, OPTIONS'); res.setHeader('Content-Type',
+                // 'application/json');
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Acc" +
+                        "ess-Control-Request-Method, Access-Control-Request-Headers");
+
+                next();
+            });
     }
 
-    private mongoSetup(): void{
-        mongoose.connect(this.mongoUrl);        
+    private mongoSetup() : void {
+        mongoose
+            .connect(this.mongoUrl)
+            .then(() => console.log("Connected to Mongo"))
+            .catch(() => console.error(err => `cannot connect to mongoDb ${err}`));
     }
 
 }
